75e151c11de04cd0d14f73eb0bda703131282126
tranarchy <150830424+tranarchy@users.noreply.github.com>
2026-02-20 16:01:37 +0100

    dont allow placing fallable blocks on blocks that have no collision

--- src/client/java/masomode/mixin/client/MultiPlayerGameModeMixin.java
+++ src/client/java/masomode/mixin/client/MultiPlayerGameModeMixin.java
@@ -9,9 +9,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Fallable;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.shapes.Shapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -36,13 +40,21 @@ public class MultiPlayerGameModeMixin {
             }
         }
 
-        if (localPlayer.level().getBlockState(blockHitResult.getBlockPos()).is(BlockTags.BEDS)) {
+        BlockState blockState = localPlayer.level().getBlockState(blockHitResult.getBlockPos());
+
+        if (blockState.is(BlockTags.BEDS)) {
             if (Common.isBloodMoon()) {
                 MainClient.mc.player.displayClientMessage(Component.literal("Sleeping is probably a bad idea now"), true);
                 callbackInfoReturnable.setReturnValue(InteractionResult.FAIL);
             }
-        } else if (localPlayer.level().getBlockState(blockHitResult.getBlockPos()).is(Blocks.LAVA_CAULDRON)) {
+        } else if (blockState.is(Blocks.LAVA_CAULDRON)) {
             callbackInfoReturnable.setReturnValue(InteractionResult.FAIL);
+        } else if (blockState.getCollisionShape(localPlayer.level(), blockHitResult.getBlockPos()) == Shapes.empty() && !blockState.canBeReplaced()) {
+            if (MainClient.mc.player.getItemInHand(interactionHand).getItem() instanceof BlockItem blockItem) {
+                if (blockItem.getBlock() instanceof Fallable) {
+                    callbackInfoReturnable.setReturnValue(InteractionResult.FAIL);
+                }
+            }
         }
     }
 }
